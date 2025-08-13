e61d17c45e053824cd2400f73543cb0ea8d8aebc
Loc Nguyen <tanlocit20062000@gmail.com>
2025-08-13 20:30:19 +0700

    done

--- thuc-hanh-reactjs/src/components/manage-task-app/manage-task-app.jsx
+++ thuc-hanh-reactjs/src/components/manage-task-app/manage-task-app.jsx
@@ -1,38 +1,133 @@
-import React from 'react'
-import IconPlus from "../../assets/images/icon-plus.svg"
-import IconThreeDots from "../../assets/images/icon-three-dots.svg"
-import "./style.css"
-import TaskItem from './task-item/task-item'
+import React, { useState } from "react";
+import IconPlus from "../../assets/images/icon-plus.svg";
+import IconThreeDots from "../../assets/images/icon-three-dots.svg";
+import "./style.css";
+import TaskItem from "./task-item/task-item";
+import { Button, Input } from "antd";
+import CreateTask from "./modal/create-task";
 
 const ManageTaskApp = () => {
+  const { Search } = Input;
+  const [modalCreateTask, setModalCreateTask] = useState(false);
+
+  const onSearch = (value, _e, info) => console.log(info?.source, value);
+
   return (
-    <div className='manage-task-app'>
-      
-      {/* todo column */}
-      <div className='todo-column'>
-        <div className='todo-column-header'>
-          <div className='todo-column-header-left'>
-            <p className='label'>Todo</p>
-            <span className='count'>3</span>
+    <>
+      <div className="container">
+        <div className="header-section">
+          <div className="header-left">
+            <Search
+              placeholder="input search text"
+              allowClear
+              onSearch={onSearch}
+              style={{ width: 304 }}
+            />
           </div>
-          <div className='todo-column-header-right'>
-            <button className='btn-plus'><img src={IconPlus} alt="" /></button>
-            <button className='btn-ellipsis'><img src={IconThreeDots} alt="" /></button>
+          <div className="header-right">
+            <Button type="primary" onClick={() => setModalCreateTask(true)}>
+              New Item
+            </Button>
           </div>
         </div>
+        <div className="manage-task-app">
+          {/* todo column */}
+          <div className="todo-column">
+            <div className="todo-column-header">
+              <div className="todo-column-header-left">
+                <p className="label">Todo</p>
+                <span className="count">3</span>
+              </div>
+              <div className="todo-column-header-right">
+                <button className="btn-plus">
+                  <img src={IconPlus} alt="" />
+                </button>
+                <button className="btn-ellipsis">
+                  <img src={IconThreeDots} alt="" />
+                </button>
+              </div>
+            </div>
 
-        <div className='todo-column-body'>
-          <TaskItem />
-          <TaskItem />
-          <TaskItem />
+            <div className="todo-column-body">
+              <TaskItem />
+              <TaskItem />
+              <TaskItem />
+            </div>
+          </div>
+
+          <div className="todo-column">
+            <div className="todo-column-header">
+              <div className="todo-column-header-left">
+                <p className="label">Todo</p>
+                <span className="count">3</span>
+              </div>
+              <div className="todo-column-header-right">
+                <button className="btn-plus">
+                  <img src={IconPlus} alt="" />
+                </button>
+                <button className="btn-ellipsis">
+                  <img src={IconThreeDots} alt="" />
+                </button>
+              </div>
+            </div>
+
+            <div className="todo-column-body">
+              <TaskItem />
+              <TaskItem />
+              <TaskItem />
+            </div>
+          </div>
+
+          <div className="todo-column">
+            <div className="todo-column-header">
+              <div className="todo-column-header-left">
+                <p className="label">Todo</p>
+                <span className="count">3</span>
+              </div>
+              <div className="todo-column-header-right">
+                <button className="btn-plus">
+                  <img src={IconPlus} alt="" />
+                </button>
+                <button className="btn-ellipsis">
+                  <img src={IconThreeDots} alt="" />
+                </button>
+              </div>
+            </div>
+
+            <div className="todo-column-body">
+              <TaskItem />
+              <TaskItem />
+              <TaskItem />
+            </div>
+          </div>
+
+          <div className="todo-column">
+            <div className="todo-column-header">
+              <div className="todo-column-header-left">
+                <p className="label">Todo</p>
+                <span className="count">3</span>
+              </div>
+              <div className="todo-column-header-right">
+                <button className="btn-plus">
+                  <img src={IconPlus} alt="" />
+                </button>
+                <button className="btn-ellipsis">
+                  <img src={IconThreeDots} alt="" />
+                </button>
+              </div>
+            </div>
+
+            <div className="todo-column-body">
+              <TaskItem />
+              <TaskItem />
+              <TaskItem />
+            </div>
+          </div>
         </div>
       </div>
+      { modalCreateTask && <CreateTask modalCreateTask={modalCreateTask} setModalCreateTask={setModalCreateTask} />}
+    </>
+  );
+};
 
-      <div className='todo-column'></div>
-      <div className='todo-column'></div>
-      <div className='todo-column'></div>
-    </div>
-  )
-}
-
-export default ManageTaskApp
+export default ManageTaskApp;
--- thuc-hanh-reactjs/src/main.jsx
+++ thuc-hanh-reactjs/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
+import '@ant-design/v5-patch-for-react-19'; 
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <>
     <App />
-  </StrictMode>,
+  </>,
 )
